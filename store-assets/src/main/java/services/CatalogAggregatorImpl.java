724d6d9d8c2986211f1e8b0c0e6d83ea2355783f
Luciano Resende <lresende@apache.org>
2010-09-10 16:32:38 +0000

    Making handling catalog references more flexible
    git-svn-id: https://svn.apache.org/repos/asf/incubator/nuvem/trunk@995858 13f79535-47bb-0310-9956-ffa450edef68

--- store-assets/src/main/java/services/CatalogAggregatorImpl.java
+++ store-assets/src/main/java/services/CatalogAggregatorImpl.java
@@ -6,18 +6,21 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package services;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.oasisopen.sca.annotation.Property;
 import org.oasisopen.sca.annotation.Reference;
@@ -34,33 +37,40 @@ public class CatalogAggregatorImpl implements Catalog {
     @Reference
     public CurrencyConverter currencyConverter;
 
-    @Reference
+    @Reference(required=false)
     public Catalog fruitsCatalog;
-    
-    @Reference
+
+    @Reference(required=false)
     public Catalog vegetablesCatalog;
 
     public Item[] get() {
         String currencySymbol = currencyConverter.getCurrencySymbol(currencyCode);
 
-        Item[] fruits = fruitsCatalog.get();
-        Item[] vegetables = vegetablesCatalog.get();
+        List<Item> catalog = new ArrayList<Item>();
+        if(fruitsCatalog != null) {
+        	Item[] fruits = fruitsCatalog.get();
 
-        Item[] catalog = new Item[fruits.length + vegetables.length];
-        int i =0;
-        for (Item item: fruits) {
-            double price = item.getPrice();  
-            price = currencyConverter.getConversion("USD", currencyCode, price);
-            catalog[i++] = new Item(item.getName(), currencyCode, currencySymbol, price);
-        }
-        
-        for (Item item: vegetables) {
-            double price = item.getPrice();  
-            price = currencyConverter.getConversion("USD", currencyCode, price);
-            catalog[i++] = new Item(item.getName(), currencyCode, currencySymbol, price);
+        	for (Item item: fruits) {
+                double price = item.getPrice();
+                price = currencyConverter.getConversion("USD", currencyCode, price);
+                catalog.add(new Item(item.getName(), currencyCode, currencySymbol, price));
+            }
         }
 
-        return catalog;
+        if(vegetablesCatalog != null) {
+        	Item[] vegetables = vegetablesCatalog.get();
+
+            for (Item item: vegetables) {
+                double price = item.getPrice();
+                price = currencyConverter.getConversion("USD", currencyCode, price);
+                catalog.add(new Item(item.getName(), currencyCode, currencySymbol, price));
+            }
+        }
+
+        Item[] catalogArray = new Item[catalog.size()];
+        catalog.toArray(catalogArray);
+
+        return catalogArray;
     }
 
 }
