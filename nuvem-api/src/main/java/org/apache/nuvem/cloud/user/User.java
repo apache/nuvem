4917f6268b59b1d1b184cb7d74e7042303891848
Luciano Resende <lresende@apache.org>
2010-09-13 16:32:55 +0000

    Adding noarg default constructor
    git-svn-id: https://svn.apache.org/repos/asf/incubator/nuvem/trunk@996593 13f79535-47bb-0310-9956-ffa450edef68

--- nuvem-api/src/main/java/org/apache/nuvem/cloud/user/User.java
+++ nuvem-api/src/main/java/org/apache/nuvem/cloud/user/User.java
@@ -6,43 +6,51 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.apache.nuvem.cloud.user;
 
-public class User {
-    public static enum ROLES { UNDEFINED, USER, ADMIN};
-    
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = -503746790472903416L;
+
+	public static enum ROLES { UNDEFINED, USER, ADMIN};
+
     protected String userId;
     protected String nickName;
     protected String email;
-    
+
+    public User () {
+
+    }
+
     public User(String userId, String nickName, String email) {
         this.userId = userId;
         this.nickName = nickName;
         this.email = email;
     }
-    
+
     public String getUserId() {
         return this.userId;
     }
-    
+
     public String getNickname() {
         return this.nickName;
     }
-    
+
     public String getEmail() {
         return this.email;
     }
-    
-  
+
+
 }
--- nuvem-api/src/main/java/org/apache/nuvem/cloud/user/UserContext.java
+++ nuvem-api/src/main/java/org/apache/nuvem/cloud/user/UserContext.java
@@ -6,46 +6,52 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.apache.nuvem.cloud.user;
 
 public class UserContext extends User {
-    protected String loginUrl;
+	private static final long serialVersionUID = 1339319191804052423L;
+
+	protected String loginUrl;
     protected String logoutUrl;
     protected boolean isUserLoggedIn;
-    
+
+    public UserContext() {
+    	super();
+    }
+
     public UserContext(String userId, String nickName, String email, boolean isUserLoggedIn, String loginUrl, String logoutUrl) {
         super(userId, nickName, email);
         this.isUserLoggedIn = isUserLoggedIn;
         this.loginUrl = loginUrl;
         this.logoutUrl = logoutUrl;
     }
-    
+
     public UserContext(User user, boolean isUserLoggedIn, String loginUrl, String logoutUrl) {
         super(user.getUserId(), user.getNickname(), user.getEmail());
         this.isUserLoggedIn = isUserLoggedIn;
         this.loginUrl = loginUrl;
         this.logoutUrl = logoutUrl;
     }
-    
+
     public boolean isUserLoggedIn() {
         return this.isUserLoggedIn;
     }
-    
+
     public String getLoginUrl() {
         return this.loginUrl;
     }
-    
+
     public String getLogoutUrl() {
         return this.logoutUrl;
     }
